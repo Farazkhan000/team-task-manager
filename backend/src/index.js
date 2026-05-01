198d16b898531f4259dc0989a1f35a32703dbbe5
Ghufran-Zaidi-05 <xaidiraza7860@gmail.com>
2026-05-01 21:51:53 +0530

    fix: remove duplicate path declaration

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,7 +26,6 @@ app.use('/api/projects', projectRoutes);
 app.use('/api/tasks', taskRoutes);
 app.use('/api/dashboard', dashboardRoutes);
 
-const path = require('path');
 
 // --------------- Error Handler ---------------
 app.use(errorHandler);
